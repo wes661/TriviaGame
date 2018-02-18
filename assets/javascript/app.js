ee4f82c6ba14992406d6f7b6bb77f1e8b8c5f080
Wesley Hanson <wrh1600@gmail.com>
2018-02-18 13:21:06 -0700

    questions now display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 var triviaGame = {
 	questions: [
 		{
-				question1: 'Who holds the record for most touchdown passes?',
+				question: 'Who holds the record for most touchdown passes?',
 				answers: [
 				{
 					a: 'Tom Brady',
@@ -22,7 +22,7 @@ var triviaGame = {
 			]
 		},
 		{		
-				question2: 'Which team has appeared in the most superbowls?',
+				question: 'Which team has appeared in the most Superbowls?',
 				answers: [
 				{
 					a: 'The New England Patriots',
@@ -41,7 +41,78 @@ var triviaGame = {
 					correct: false
 				}
 			]
-		}
-	]
-}
-console.log(triviaGame.questions[1])
+		},
+		{		
+				question: 'Who holds the record for most rushing yards in a season?',
+				answers: [
+				{
+					a: 'Adrian Peterson',
+					correct: false
+				},
+				{
+					b: 'Barry Sanders',
+					correct: false
+				},
+				{
+					c: 'Eric Dickerson',
+					correct: true
+				},
+				{
+					d: 'Chris Johnson',
+					correct: false
+				}
+			]
+		},
+		{		
+				question: 'How many teams have never been to the Superbowl?',
+				answers: [
+				{
+					a: '6',
+					correct: false
+				},
+				{
+					b: '4',
+					correct: true
+				},
+				{
+					c: '8',
+					correct: false
+				},
+				{
+					d: '3',
+					correct: false
+				}
+			]
+		},
+		{		
+				question: 'Who played in the first Superbowl?',
+				answers: [
+				{
+					a: 'Green Bay Packers vs Oakland Raiders',
+					correct: false
+				},
+				{
+					b: 'Dallas Cowboys vs Miami Dolphins',
+					correct: false
+				},
+				{
+					c: 'New York Jets vs Baltimore Colts',
+					correct: false
+				},
+				{
+					d: 'Green Bay Packers vs Kansas City Chiefs',
+					correct: true
+				}
+			]
+		},
+	],
+	gameStart: function(){
+		this.randQuestion = this.questions[Math.floor(Math.random() * this.questions.length)];
+		$('.questionBox').text(this.randQuestion.question);
+		$('.questionBox').text(this.randQuestion.question);
+		
+	}
+};
+triviaGame.gameStart();
+console.log(triviaGame.randQuestion.question.answers);
+//alert(triviaGame.randQuestion);
