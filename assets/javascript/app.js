f59590ceba14729f02b58fe55e606e3d114022c9
Wesley Hanson <wrh1600@gmail.com>
2018-02-19 20:35:46 -0700

    added more questions working on click function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -67,22 +67,22 @@ var triviaGame = {
 			]
 		},
 		{		
-				question: 'How many teams have never been to the Superbowl?',
+				question: 'Which team has never been to the Superbowl?',
 				answers: [
 				{
-					a: 'A: 6',
+					a: 'A: Tampabay Buccaneers',
 					correct: false
 				},
 				{
-					b: 'B: 4',
+					b: 'B: Clevland Browns',
 					correct: true
 				},
 				{
-					c: 'C: 8',
+					c: 'C: Tennessee Titans',
 					correct: false
 				},
 				{
-					d: 'D: 3',
+					d: 'D: Arizona Cardinals',
 					correct: false
 				}
 			]
@@ -105,6 +105,48 @@ var triviaGame = {
 				{
 					d: 'D: Green Bay Packers vs Kansas City Chiefs',
 					correct: true
+				}
+			]
+		},
+		{		
+				question: 'Who holds the record for most receiving yards',
+				answers: [
+				{
+					a: 'A: Jerry Rice',
+					correct: true
+				},
+				{
+					b: 'B: Larry Fitzgerald',
+					correct: false
+				},
+				{
+					c: 'C: Tony Gonzalez',
+					correct: false
+				},
+				{
+					d: 'D: Randy Moss',
+					correct: false
+				}
+			]
+		},
+		{		
+				question: 'How many NFL teams are there?',
+				answers: [
+				{
+					a: 'A: 30',
+					correct: false
+				},
+				{
+					b: 'B: 32',
+					correct: true
+				},
+				{
+					c: 'C: 28',
+					correct: false
+				},
+				{
+					d: 'D: 36',
+					correct: false
 				}
 			]
 		},
@@ -137,17 +179,29 @@ var triviaGame = {
 
       clearInterval(intervalId);
     }
-    run();
+    	run();
 
 
 		//Random question select and display to html
 		});
 		this.randQuestion = this.questions[Math.floor(Math.random() * this.questions.length)];
 		$('#questionTitle').append(this.randQuestion.question + '<br>' + '<br>');
-		$('#answerSpot').append(this.randQuestion.answers[0].a + '<br>' + '<br>');
-		$('#answerSpot').append(this.randQuestion.answers[1].b + '<br>' + '<br>');
-		$('#answerSpot').append(this.randQuestion.answers[2].c + '<br>' + '<br>');
-		$('#answerSpot').append(this.randQuestion.answers[3].d);
-	}
+		$('#answerSpotA').append(this.randQuestion.answers[0].a + '<br>' + '<br>');
+		$('#answerSpotB').append(this.randQuestion.answers[1].b + '<br>' + '<br>');
+		$('#answerSpotC').append(this.randQuestion.answers[2].c + '<br>' + '<br>');
+		$('#answerSpotD').append(this.randQuestion.answers[3].d);
+
+		function selectAnswer(){
+			for(var i = 0; i < questions.length; i++ ){
+				answers[i].click(function(){
+					alert('selected');
+					console.log(answers);
+				})
+			}
+		}
+		selectAnswer();
+	},
 };
 triviaGame.gameStart();
+
+console.log(triviaGame);
