704374754c26186d29fada0f81b29883664bd2c4
Wesley Hanson <wrh1600@gmail.com>
2018-02-23 21:20:34 -0700

    styling added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # TriviaGame
+
+This is an NFL javascript trivia game that runs on a timer.
+Randomly selects a new question if your timer runs out or you guess correctly or incorrectly.
+It displays "Correct" "Incorrect" or "Times Up" and keeps score.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,10 @@
-$('.jtron').slideDown(1800);
+$('.jumbotron').slideDown(1800);
 var number = 15;
 var score = 0;
+var audio1 = document.getElementById('nflSound');
+var correctCount = 0;
+var incorrectCount = 0;
+var questionCount = 0;
 
 //Object for game
 var triviaGame = {
@@ -156,10 +160,13 @@ var triviaGame = {
 //Game starts with this function. Displayes question, timer and answers	
 	gameStart: function(){
 		$('.btn-info').on('click', function(){
+			audio1.play();
 			$('.questionBox').slideDown('slow');
 			$('.scoreWord').slideDown('slow');
 			$('.scoreBoard').slideDown('slow');
 			$('.btn-info').slideUp('slow');
+			questionCount ++;
+			console.log(questionCount);
 			//Timer function			
 			var intervalId;
 			function run() {
@@ -173,14 +180,17 @@ var triviaGame = {
 	      $("#timer").html(number);
 
 	      if (number === 0) {
+	      	questionCount ++;
+	      	incorrectCount ++;
+	      	console.log(questionCount);
+	      	console.log(incorrectCount);
 	      	number = 18;
-	      	$('.questionBox').slideUp(100)
-	      	$('.timeupText').slideDown(1500);
-			$('.timeupText').slideUp(1500);
+	      	$('.questionBox').hide(100)
 	      	stop();
 	      	setTimeout(triviaGame.threeSeconds, 1000 * 3);
 	      	$("#timer").html(number);
 	      	run();
+	      	triviaGame.gameResult();
 	   		}
     	}
 
@@ -194,56 +204,71 @@ var triviaGame = {
     	});
 	},
 		//Random question select and display to html
-		
-		
-		questionSelect: function(){
-			var that = this;
-			that.randIndex = Math.floor(Math.random() * that.questions.length);
-			that.randQuestion = that.questions[that.randIndex];
-			$('#questionTitle').html(that.randQuestion.question + '<br>' + '<br>');
-			$('#answerSpotA').html(that.randQuestion.answers[0].text);
-			$('#answerSpotA').attr("correct", that.randQuestion.answers[0].correct);
-			$('#answerSpotB').html(that.randQuestion.answers[1].text);
-			$('#answerSpotB').attr("correct", that.randQuestion.answers[1].correct);
-			$('#answerSpotC').html(that.randQuestion.answers[2].text);
-			$('#answerSpotC').attr("correct", that.randQuestion.answers[2].correct);
-			$('#answerSpotD').html(that.randQuestion.answers[3].text);
-			$('#answerSpotD').attr("correct", that.randQuestion.answers[3].correct);
-		},
+	questionSelect: function(){
+		var that = this;
+		that.randIndex = Math.floor(Math.random() * that.questions.length);
+		that.randQuestion = that.questions[that.randIndex];
+		$('#questionTitle').html(that.randQuestion.question + '<br>' + '<br>');
+		$('#answerSpotA').html(that.randQuestion.answers[0].text);
+		$('#answerSpotA').attr("correct", that.randQuestion.answers[0].correct);
+		$('#answerSpotB').html(that.randQuestion.answers[1].text);
+		$('#answerSpotB').attr("correct", that.randQuestion.answers[1].correct);
+		$('#answerSpotC').html(that.randQuestion.answers[2].text);
+		$('#answerSpotC').attr("correct", that.randQuestion.answers[2].correct);
+		$('#answerSpotD').html(that.randQuestion.answers[3].text);
+		$('#answerSpotD').attr("correct", that.randQuestion.answers[3].correct);
+		console.log(triviaGame.randQuestion);
+	},
 
 		
+	//Time out function between questions
+	threeSeconds: function(){
+		triviaGame.questions.splice(triviaGame.randIndex,1);
+		triviaGame.questionSelect();
+		$('.questionBox').slideDown(100);
+	},
+	//Selecting answer function
+	selectAnswer: function(){
+		$('.answers').on('click', function(){
+			var val = $(this).attr('correct')
+			if ( val == 'true'){
+				questionCount ++;
+				correctCount ++;
+				number = 18;
+      			$('.questionBox').hide(100)
+      			$('.correctText').slideDown(1500);
+				$('.correctText').slideUp(1500);
+      			setTimeout(triviaGame.threeSeconds, 1000 * 3);
+      			$("#timer").html(number);
+      			score ++;
+      			$('.scoreBoard').html('<center>' + '<h1>' + score + '</h1>' + '</center>');
+      			triviaGame.gameResult();
+        	}
 
-		threeSeconds: function(){
-			triviaGame.questions.splice(triviaGame.randIndex,1);
-			triviaGame.questionSelect();
-			$('.questionBox').slideDown(100);
-		},
+			else{
+				questionCount ++;
+				incorrectCount ++;
+				console.log(incorrectCount);
+      			console.log(questionCount);
+				number = 18;
+				$('.questionBox').hide(100);
+				$('.incorrectText').slideDown(1500);
+				$('.incorrectText').slideUp(1500);
+				setTimeout(triviaGame.threeSeconds, 1000 * 3);
+				$("#timer").html(number);
+				triviaGame.gameResult();
+			}
+		})
+	},
 
-		selectAnswer: function(){
-			$('.answers').on('click', function(){
-				var val = $(this).attr('correct')
-				if ( val == 'true'){
-					number = 18;
-	      			$('.questionBox').slideUp(100)
-	      			$('.correctText').slideDown(1500);
-					$('.correctText').slideUp(1500);
-	      			setTimeout(triviaGame.threeSeconds, 1000 * 3);
-	      			$("#timer").html(number);
-	      			score ++;
-	      			$('.scoreBoard').html('<center>' + '<h1>' + score + '</h1>' + '</center>');
-	        	}
-
-				else{
-					number = 18;
-					$('.questionBox').slideUp(100);
-					$('.incorrectText').slideDown(1500);
-					$('.incorrectText').slideUp(1500);
-					setTimeout(triviaGame.threeSeconds, 1000 * 3);
-					$("#timer").html(number);
-				}
-			})
-		},
-	};
+	gameResult: function(){
+		if(questionCount === 7){
+			$('#results').text('Correct: ' + correctCount + '<br>');
+			$('#results').text('Incorrect: ' + incorrectCount);
+		}
+	},
+};
 triviaGame.selectAnswer();	
 triviaGame.gameStart();
+
 //console.log(triviaGame);
